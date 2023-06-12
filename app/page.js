7bdf9f173e55feffcf604de78a1179ddf43c3ce8
danielAwale <danielmadhukarawale@gmail.com>
2023-06-12 07:24:05 -0700

    center the title and fix padding

--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1>Expense Tracker</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-4">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm">
+        <h1 className='text-4xl p-4 text-center'>Expense Tracker</h1>
       </div>
     </main>
   )
