1e71a871f5c9941ecdf186f980b945c67e60625c
danielAwale <danielmadhukarawale@gmail.com>
2023-06-21 13:27:56 -0700

    import and export getfirestore

--- app/firebase.js
+++ app/firebase.js
@@ -3,6 +3,8 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+import { getFirestore } from "firebase/firestore"
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC25RrVLiFJkCJOpTyiK1tvuH152HP-kM0",
@@ -15,3 +17,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app)
