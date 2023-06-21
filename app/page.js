a01745a3893b11b7a21d115d37f2525d375e3e26
danielAwale <danielmadhukarawale@gmail.com>
2023-06-21 14:31:52 -0700

    add delete functionality and finish the project

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState, useEffect } from "react"
-import { collection, addDoc, getDoc, QuerySnapshot, onSnapshot, query } from "firebase/firestore";
+import { collection, addDoc, getDoc, QuerySnapshot, deleteDoc, onSnapshot, query, doc } from "firebase/firestore";
 import { db } from "./firebase";
 
 
@@ -51,6 +51,11 @@ export default function Home() {
 
   // Delete items from database
 
+  const deleteItem = async (id) => {
+    await deleteDoc(doc(db, "items", id))
+  }
+
+
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-4">
@@ -69,7 +74,7 @@ export default function Home() {
                   <span className="capitalize">{item.name}</span>
                   <span>{item.price}</span>
                 </div>
-                <button className="ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16">X</button>
+                <button onClick={() => deleteItem(item.id)} className="ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16">X</button>
               </li>
             ))}
           </ul>
