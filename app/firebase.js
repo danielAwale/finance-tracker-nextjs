38edd299d042349910f3a8fcea008ef33749b625
danielAwale <danielmadhukarawale@gmail.com>
2023-06-21 13:25:42 -0700

    initialize firebase

---
+++ app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC25RrVLiFJkCJOpTyiK1tvuH152HP-kM0",
+  authDomain: "expense-tracker-4f111.firebaseapp.com",
+  projectId: "expense-tracker-4f111",
+  storageBucket: "expense-tracker-4f111.appspot.com",
+  messagingSenderId: "136298383689",
+  appId: "1:136298383689:web:4558fa11e47c926621b298"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
